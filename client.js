953ade23d494b7ecb6da2154423df975d6574af3
Emil Hesslow <hesslow@gmail.com>
2010-09-07 00:19:45 +0200

    Some work on getting it to work with node 0.2.0

--- client.js
+++ client.js
@@ -1,11 +1,10 @@
 var fs = require("fs");
 var sys = require("sys");
-var tcp = require("tcp");
 var bittorrent = require("./lib/bittorrent");
 
 // Create client and start listening on port 80.
 var client = new bittorrent.Client();
-client.listen(80, "localhost");
+client.listen(13614, "192.168.2.2"); // 
 
 // Load a torrent and add it to the client
 var t = new bittorrent.Torrent("test.torrent", ".");
